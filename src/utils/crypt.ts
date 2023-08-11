74edbe664def2c9ebdae419aa58dfb66d7492f5e
yuriduque-ciandt <yurithielmann83@gmail.com>
2023-08-11 09:29:30 -0300

    creating crypt utils

---
+++ src/utils/crypt.ts
@@ -0,0 +1,13 @@
+import bcrypt from "bcrypt";
+
+export async function encrypt(text) {
+  const salt = await bcrypt.genSalt(10);
+  const result = await bcrypt.hash(text, salt);
+
+  return result;
+}
+
+export async function decrypt(password) {
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(password, salt);
+}
