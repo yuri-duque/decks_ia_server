854a5696051dbe18e6e191f1bb9094911767b0e0
yuriduque-ciandt <yurithielmann83@gmail.com>
2023-08-11 09:30:52 -0300

    updating server

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ import bodyParser from "body-parser";
 
 import customResponse from "./utils/customResponse";
 import defaultController from "./controllers/defaultController";
+import authController from "./controllers/authController";
+import userController from "./controllers/userController";
 
 dotenv.config();
 
@@ -17,15 +19,17 @@ app.use(express.static("public"));
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors({ origin: ["http://localhost:3001", "https://gym-server-i9wq.onrender.com"] }));
+app.use(cors({ origin: ["http://localhost:3001"] }));
 app.use(customResponse);
 
 app.use("", defaultController);
+app.use("/api", authController);
+app.use("/api", userController);
 
 app.use(function (req, res) {
-  res.status(404).json({ message: "Erro ao acessar a rota" });
+  res.status(404).json({ message: "Erro ao get touter" });
 });
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando com sucesso ${HOSTNAME}:${PORT}/docs`);
+  console.log(`Server is running on ${HOSTNAME}:${PORT}/docs`);
 });
